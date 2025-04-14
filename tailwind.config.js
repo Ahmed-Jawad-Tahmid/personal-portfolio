32c5bcef2e13e6edab3bce3bf585d125cd8b1bf8
Ahmed-Jawad-Tahmid <105600921+Ahmed-Jawad-Tahmid@users.noreply.github.com>
2025-04-14 15:14:03 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    darkMode: 'class', // Enable dark mode via class toggling
+    content: [
+      "./index.html",
+      "./js/**/*.js",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
