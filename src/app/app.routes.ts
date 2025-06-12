ab48ea665a7d45dba9b66be20382cd84654056b5
Ahmed-Jawad-Tahmid <105600921+Ahmed-Jawad-Tahmid@users.noreply.github.com>
2025-06-12 02:02:47 -0600

    Restart

--- src/app/app.routes.ts
+++
@@ -1,5 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-    {path : '', loadChildren: () => import('./portfolio-website/portfolio-website.module').then(m => m.PortfolioWebsiteModule)}
-];
