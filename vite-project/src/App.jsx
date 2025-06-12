9119228ff0747405b951b60f9f2bb571ee4f892e
Ahmed-Jawad-Tahmid <105600921+Ahmed-Jawad-Tahmid@users.noreply.github.com>
2025-06-12 02:51:02 -0600

    Fixed some errors

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter, Route } from "react-router-dom"
-import { Home } from "./pages/Home"
-import { NotFound } from "./pages/NotFound"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
 
   return (
     <><BrowserRouter>
       <Routes>
-        <Route index element={<Home />}/>
+        <Route index element={<Home />} />
         <Route path="*" element ={<NotFound />}/>
 
       </Routes>
